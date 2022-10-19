49229f41881990959f38349fce0c70ab62e64d9b
mdijas <116139042+mdijas@users.noreply.github.com>
2022-10-19 10:04:03 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,19 @@
+package com.basic;
+
+public class Basic {
+	public static void main(String[] args) {
+		System.out.println("Hai");
+		System.out.println("---->" +args[0]);
+		System.out.println(args[3]);
+		System.out.println(args[4]);
+		int a =10;
+		System.out.println(a);
+		int b = 15;
+		System.out.println(b);
+		System.out.println("Addition =" + a);
+		
+		
+		
+	}
+
+}
